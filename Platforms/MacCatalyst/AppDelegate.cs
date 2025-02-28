c994b5e891f5d04c1ae32758937542a88805dfc8
Lara904 <lara.oueriagli@gmail.com>
2025-02-28 09:11:54 +0100

    Add files via upload

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HealthAnalyz
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
